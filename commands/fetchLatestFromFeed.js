79d6b465e866e9b697c9248db2204524fb3cc881
justrhoto <otisub@gmail.com>
2024-12-13 10:57:25 -0600

    Basic refresh and post functionality

--- commands/fetchLatestFromFeed.js
+++ commands/fetchLatestFromFeed.js
@@ -2,23 +2,39 @@ const { SlashCommandBuilder } = require('discord.js');
 const Parser = require('rss-parser');
 
 const parser = new Parser();
+let lastFeedItemUrl = null;
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('fetch')
-		.setDescription('Fetch latest post from RSS feed at URL')
+		.setName('add-feed')
+		.setDescription('Add feed to channel')
         .addStringOption(option => option
             .setName('rss_url')
             .setDescription('The RSS feed URL')
             .setRequired(true)
         ),
 	async execute(interaction) {
-        console.log(interaction.options.getString('rss_url'));
+        const rssUrl = interaction.options.getString('rss_url');
+        console.log(rssUrl);
         await interaction.deferReply({ ephemeral: true });
+        const feed = await parser.parseURL(rssUrl);
 
-        const feed = await parser.parseURL(interaction.options.getString('rss_url'));
+        if (!feed || feed.items.length === 0) {
+            await interaction.editReply('Invalid or empty RSS feed provided.');
+            return;
+        }
 
-		const firstItemUrl = feed.items[0].content.match(/https?:\/\/.*?.jpg/)[0];
-		await interaction.editReply(`${firstItemUrl}`);
+        setInterval(async () => {
+            const feed = await parser.parseURL(rssUrl);
+            const firstItemUrl = feed.items[0].content.match(/https?:\/\/.*?.jpg/)[0];
+
+            if (firstItemUrl !== lastFeedItemUrl) {
+                lastFeedItemUrl = firstItemUrl;
+                await interaction.channel.send(`New ${feed.title} post! <${feed.items[0].link}>`);
+                await interaction.channel.send(`${firstItemUrl}`);
+            }
+        }, 900000);
+
+        await interaction.editReply(`Feed ${feed.title} added to ${interaction.channel}`);
 	},
 };

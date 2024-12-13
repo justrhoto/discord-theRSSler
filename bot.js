a141b82b108fd8e9076046217bdb7b91c99e172c
justrhoto <otisub@gmail.com>
2024-12-12 22:50:47 -0600

    Cog loading log

--- bot.js
+++ bot.js
@@ -16,6 +16,7 @@ for (const file of commandFiles) {
     const command = require(filePath);
     if ('data' in command && 'execute' in command) {
         client.commands.set(command.data.name, command);
+        console.log(`Cog loaded: ${command.data.name}`);
     } else {
         console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`);
     }

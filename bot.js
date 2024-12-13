478210fb1b20927991efc1bf2e65f81f4a49522d
justrhoto <otisub@gmail.com>
2024-12-12 22:29:28 -0600

    setup to use command modules

--- bot.js
+++ bot.js
@@ -1,9 +1,25 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import { config } from 'dotenv';
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Events, GatewayIntentBits, Collection, MessageFlags } = require('discord.js');
+const { config } = require('dotenv');
 
 config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`);
+    }
+}
 
 client.on(Events.ClientReady, readyClient => {
   console.log(`Logged in as ${readyClient.user.tag}!`);
@@ -11,9 +27,22 @@ client.on(Events.ClientReady, readyClient => {
 
 client.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({ content: 'There was an error while executing tyhis command!', flags: MessageFlags.Ephemeral });
+    } else {
+        await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+    }
   }
 });
 

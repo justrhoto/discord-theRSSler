7fe96e9e57294452329db27cfb0ba6d049563e8a
justrhoto <otisub@gmail.com>
2024-12-15 21:25:27 -0600

    init modules when needed

--- bot.js
+++ bot.js
@@ -16,6 +16,9 @@ for (const file of commandFiles) {
     const command = require(filePath);
     if ('data' in command && 'execute' in command) {
         client.commands.set(command.data.name, command);
+        if (command.init) {
+            command.init(client);
+        }
         console.log(`Cog loaded: ${command.data.name}`);
     } else {
         console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`);

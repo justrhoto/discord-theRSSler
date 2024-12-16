e5d895b2a1599ee3835fa3de83c45d4aec2944c2
justrhoto <otisub@gmail.com>
2024-12-16 14:24:40 -0600

    admin cog init

---
+++ commands/admin.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js')
+
+const cogs = require('../cogs')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('reload')
+        .setDescription('Reloads a command cog.')
+        .addStringOption(option => option
+            .setName('cog-name')
+            .setRequired(true)
+        ),
+    async execute(interaction) {
+        interaction.deferReply({ ephemeral: true });
+
+        if (interaction.user.id != process.env.ADMIN_USER_ID) {
+            interaction.editReply('You must be an admin to run this command.');
+            return;
+        }
+
+        const cogName = interaction.options.getString('cogName', true);
+        const cogPath = interaction.client.cogs.get(cogName);
+
+        if (!cogPath) {
+            return interaction.editReply(`There is no such cog loaded: \`${cogName}\`.`);
+        }
+
+        delete require.cache[require.resolve(cogPath)];
+        cogs.initializeCog(cogPath, interaction.client);
+        interaction.editReply(`Cog ${cogName} reloaded!`);
+    }
+}

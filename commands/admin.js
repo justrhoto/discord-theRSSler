de74cffe8db3eb77077934b41a6c9817372e0a63
justrhoto <otisub@gmail.com>
2024-12-17 15:10:46 -0600

    load and unload cog commands

--- commands/admin.js
+++ commands/admin.js
@@ -3,32 +3,92 @@ const { SlashCommandBuilder } = require('discord.js')
 const cogs = require('../cogs')
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('reload')
-        .setDescription('Reloads a command cog.')
-        .addStringOption(option => option
-            .setName('cog_name')
-            .setDescription('The name of the cog you want to reload')
-            .setRequired(true)
-        ),
-    async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true });
+    commands: [{
+        data: new SlashCommandBuilder()
+            .setName('reload')
+            .setDescription('Reloads a command cog.')
+            .addStringOption(option => option
+                .setName('cog_name')
+                .setDescription('The name of the cog you want to reload')
+                .setRequired(true)
+            ),
+        async execute(interaction) {
+            await interaction.deferReply({ ephemeral: true });
 
-        if (interaction.user.id != process.env.ADMIN_USER_ID) {
-            await interaction.editReply('You must be an admin to run this command.');
-            return;
+            if (interaction.user.id != process.env.ADMIN_USER_ID) {
+                await interaction.editReply('You must be an admin to run this command.');
+                return;
+            }
+
+            const cogName = interaction.options.getString('cog_name', true);
+            const cogPath = interaction.client.cogs.get(cogName);
+
+            if (!cogPath) {
+                await interaction.editReply(`There is no such cog loaded: \`${cogName}\`.`);
+                return;
+            }
+
+            delete require.cache[require.resolve(cogPath)];
+            cogs.initializeCog(cogPath, interaction.client);
+            await interaction.editReply(`Cog ${cogName} reloaded!`);
         }
+    },
+    {
+        data: new SlashCommandBuilder()
+            .setName('unload')
+            .setDescription('Unload a command cog.')
+            .addStringOption(option => option
+                .setName('cog_name')
+                .setDescription('The cog that you want to unload')
+                .setRequired(true)
+            ),
+        async execute(interaction) {
+            await interaction.deferReply({ ephemeral: true });
 
-        const cogName = interaction.options.getString('cog_name', true);
-        const cogPath = interaction.client.cogs.get(cogName);
+            if (interaction.user.id != process.env.ADMIN_USER_ID) {
+                await interaction.editReply('You must be an admin to run this command.');
+                return;
+            }
 
-        if (!cogPath) {
-            await interaction.editReply(`There is no such cog loaded: \`${cogName}\`.`);
-            return;
+            const cogName = interaction.options.getString('cog_name', true);
+            const cogPath = interaction.client.cogs.get(cogName);
+
+            if (!cogPath) {
+                await interaction.editReply(`There is no such cog loaded: \`${cogName}\`.`);
+                return;
+            }
+
+            delete require.cache[require.resolve(cogPath)];
+            await interaction.editReply(`Cog ${cogName} unloaded.`);
         }
+    },
+    {
+        data: new SlashCommandBuilder()
+            .setName('load')
+            .setDescription('Load a command cog.')
+            .addStringOption(option => option
+                .setName('cog_name')
+                .setDescription('The cog that you want to load')
+                .setRequired(true)
+            ),
+        async execute(interaction) {
+            await interaction.deferReply({ ephemeral: true });
 
-        delete require.cache[require.resolve(cogPath)];
-        cogs.initializeCog(cogPath, interaction.client);
-        await interaction.editReply(`Cog ${cogName} reloaded!`);
-    }
+            if (interaction.user.id != process.env.ADMIN_USER_ID) {
+                await interaction.editReply('You must be an admin to run this command.');
+                return;
+            }
+
+            const cogName = interaction.options.getString('cog_name', true);
+            const cogPath = interaction.client.cogs.get(cogName);
+
+            if (cogPath) {
+                await interaction.editReply(`Cog is already loaded: \`${cogName}\`.`);
+                return;
+            }
+
+            cogs.initializeCog(cogPath, interaction.client);
+            await interaction.editReply(`Cog ${cogName} loaded.`);
+        }
+    }]
 }
